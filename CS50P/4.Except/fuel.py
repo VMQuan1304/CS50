aab87a3f6744bf6d0ca606e56e5cf33f24e74612
vuminhquan <vuminhquan1304@gmail.com>
2024-01-08 22:36:41 +0700

    upate

--- CS50P/4.Except/fuel.py
+++ CS50P/4.Except/fuel.py
@@ -4,17 +4,23 @@ def main():
             numerator, denominator = input("Fraction: ").split("/")
             numerator, denominator = int(numerator), int(denominator)
             fraction = (numerator / denominator) * 100
+            if fraction > 100:
+                continue
+            print(convert_value(fraction))
             break
         except (ValueError, ZeroDivisionError):
             pass
-    if fraction <= 1:
-        fraction = "E"
-    elif fraction >= 99:
-        fraction = "F"
-    else:
-        fraction = str(int(fraction)) + "%"
-    
-    print(f"{fraction}")
+        
+
+
+def convert_value(fraction):
+    if 0 <= fraction <= 1:
+        return "E"
+    elif 99 <= fraction <= 100:
+        return "F"
+    elif 1 < fraction < 99:
+        return str(int(round(fraction))) + "%"
+
 
 if __name__ == "__main__":
     main()
