2606bf2a670b1f282dc53de16f4def9674d7839c
vuminhquan <vuminhquan1304@gmail.com>
2024-01-16 20:54:06 +0700

    update

--- learning.py
+++ learning.py
@@ -1,15 +1,13 @@
+import requests
+import sys
+
 def main():
-    print_square(3)
-
-def print_columns(height: int = 1):
-    print("#\n" * height, end="")
-
-def print_row(width: int = 1):
-    print("#" * width)
-
-def print_square(size):
-    for _ in range(size):
-        print_row(4)
+    user_input = input("Input: ")
+    
+    
 
 if __name__ == "__main__":
     main()
+
+
+
