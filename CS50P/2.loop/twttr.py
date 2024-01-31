e5a54ff2be00488e3e90b278d3e53a39b36f98f3
vuminhquan <vuminhquan1304@gmail.com>
2024-01-31 22:11:03 +0700

    update

--- CS50P/2.loop/twttr.py
+++ CS50P/2.loop/twttr.py
@@ -1,8 +1,8 @@
 def main():
     user_input: str = input("Input: ")
-    print("Output: ", vowels_omit(user_input))
+    print("Output: ", shorten(user_input))
 
-def vowels_omit(letters: str) -> str:
+def shorten(letters: str) -> str:
     letters_omitted = ""
     for letter in letters:
         if letter.lower() not in set("aeiou"):
