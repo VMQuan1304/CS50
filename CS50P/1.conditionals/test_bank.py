7cbf82de0ebdee4fb614f86849cd2447099a00ec
vuminhquan <vuminhquan1304@gmail.com>
2024-02-22 22:41:30 +0700

    update

---
+++ CS50P/1.conditionals/test_bank.py
@@ -0,0 +1,11 @@
+from bank import value
+
+def test_value_1():
+    assert value("Hello") == 0
+    assert value("Hello ") == 0
+def test_value_2():
+    assert value("Hello, Newman") == 0
+    assert value("How you doing?") == 20
+def test_value_3():
+    assert value("What's happening?") == 100
+    assert value("What's up?") == 100
